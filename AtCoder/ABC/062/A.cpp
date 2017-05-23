079ea368dbe438c82e48d11ff88f8bea12fa3e53
mkan0141 <kanzume0141@gmail.com>
2017-05-23 22:49:51 +0900

    Update A.cpp

--- AtCoder/ABC/062/A.cpp
+++ AtCoder/ABC/062/A.cpp
@@ -1,24 +1,40 @@
 #include <iostream>
-#include <string>
 #include <algorithm>
-#include <queue>
-#include <stack>
-#include <map>
+#include <string>
 #include <vector>
 #include <cstdio>
 #include <cmath>
-#include <cctype>
+#include <cstdlib>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <utility>
 #define INF 1000000000
+#define mod 1000000007
+#define rep(i,n) for(int i=0;i<(n);i++)
+#define REP(i,a,b) for(int i=(a);i<(b);i++)
+#define all(x) (x).begin(),x.end()
 using namespace std;
+int dx[4]={1,0,-1,0};
+int dy[4]={0,1,0,-1};
 bool debug=false;
 
 int main(){
-  int a,b,c;
-  cin>>a>>b>>c;
-  if(a<=c&&c<=b){
-    cout<<"Yes"<<endl;
-  }else{
-    cout<<"No"<<endl;
+  int a[7]={1,3,5,7,8,10,12};
+  int b[4]={4,6,9,11};
+  int xx=0,yy=0;
+  int x,y;
+  cin>>x>>y;
+  for(int i=0;i<7;i++){
+    if(a[i]==x)xx=1;
+    if(a[i]==y)yy=1;
   }
+  for(int i=0;i<4;i++){
+    if(b[i]==x)xx=2;
+    if(b[i]==y)yy=2;
+  }
+  if(xx==yy)cout<<"Yes"<<endl;
+  else cout<<"No"<<endl;
   return 0;
 }
