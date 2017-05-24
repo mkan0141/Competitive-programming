f1fcb024c0d87d330049308da3d174e78e52e945
mkan0141 <kanzume0141@gmail.com>
2017-05-24 09:11:59 +0900

    hoge

---
+++ AtCoder/ABC/058/A.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <set>
+#include <vector>
+#include <cmath>
+#include <cctype>
+#define rep(i,a,b) for(int i=(a);i<b;i++)
+#define INF 1000000000
+using namespace std;
+bool debug=false;
+ 
+int main(){
+  int a,b,c;
+  cin>>a>>b>>c;
+  if(b-a==c-b)cout<<"YES"<<endl;
+  else cout<<"NO"<<endl;
+ 
+  return 0;
+}
