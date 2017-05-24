09ce939a96e4bfc4c625537e3bf78ac78125f371
mkan0141 <kanzume0141@gmail.com>
2017-05-24 09:16:39 +0900

    hoge

---
+++ AtCoder/ABC/056/A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <cstdio>
+#include <cmath>
+#define rep(i,a,b) for(int (i)=(a);i<(b);i++)
+#define INF 1000000000
+#define MAX_N 100005
+using namespace std;
+ 
+int main(){
+    char a,b;
+    cin>>a>>b;
+    if(a==b)cout<<'H'<<endl;
+    else cout<<'D'<<endl;
+    return 0;
+}
