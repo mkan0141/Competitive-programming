1a2eec86d5b2cd2debe33d2a1219978401524131
mkan0141 <kanzume0141@gmail.com>
2017-07-17 23:56:04 +0900

    Create A.cpp

---
+++ AtCoder/ABC/067/A.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <utility>
+#include <ctime>
+#define INF 999999999
+#define mod 1000000007
+ 
+#define rep(i,n) for(int i=0;i<int(n);i++)
+#define rrep(i,n) for(int i=(n);i>=0;i--)
+#define REP(i,a,b) for(int i=(a);i<int(b);i++)
+#define all(x) (x).begin(),x.end()
+#define pb push_back
+#define mp make_pair
+#define MOD(x) (x%(mod))
+using namespace std;
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+ 
+int dx[4]={1,0,-1,0};
+int dy[4]={0,1,0,-1};
+ 
+bool debug=false;
+/*---------------------------------------------------*/
+ 
+int main(){
+  int a,b,c;
+  cin>>a>>b;
+  c=a+b;
+  if(a%3==0||b%3==0||c%3==0)cout<<"Possible"<<endl;
+  else cout<<"Impossible"<<endl;
+  return 0;
+}
