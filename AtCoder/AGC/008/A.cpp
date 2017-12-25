fdf314619b0567eea93d0c3824fba8b22c09d1ad
mkan0141 <kanzume0141@gmail.com>
2017-12-25 22:55:26 +0900

    huga

---
+++ AtCoder/AGC/008/A.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <ctime>
+#include <list>
+#include <numeric>
+#include <utility>
+#include <ctime>
+#define INF 100000000
+#define LINF 9000000000000000000
+#define mod 1000000007
+ 
+#define rep(i,n) for(int i=0;i<int(n);i++)
+#define REP(i,a,b) for(int i=(a);i<int(b);i++)
+#define all(x) (x).begin(),x.end()
+#define pb push_back
+#define mp make_pair
+#define MOD(x) (x%(mod))
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+ 
+int dx[4]={1,0,-1,0};
+int dy[4]={0,1,0,-1};
+bool debug=false;
+ 
+/*---------------------------------------------------*/
+
+int main(){
+  ll x,y;
+  cin>>x>>y;
+  ll ans=LINF;
+  rep(i,2){
+    rep(j,2){
+      ll xx=(i==0?x:-x);
+      ll yy=(j==0?y:-y);
+      if(xx<=yy)ans=min(ans,yy-xx+i+j);
+    }
+  }
+  cout<<ans<<endl;
+  return 0;
+}
