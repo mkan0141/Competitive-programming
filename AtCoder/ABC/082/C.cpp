59630ec854c3302250b17141845be03bded02afd
mkan0141 <kanzume0141@gmail.com>
2017-12-26 22:17:01 +0900

    add ABC082 A,B,C

---
+++ AtCoder/ABC/082/C.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <ctime>
+#include <list>
+#include <numeric>
+#include <utility>
+#include <ctime>
+#define INF 1000000000
+#define LINF 9000000000000000000
+#define mod 1000000007
+ 
+#define rep(i,n) for(int i=0;i<int(n);i++)
+#define REP(i,a,b) for(int i=(a);i<int(b);i++)
+#define all(x) (x).begin(),x.end()
+#define pb push_back
+#define mp make_pair
+#define MOD(x) (x%(mod))
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+ 
+int dx[4]={1,0,-1,0};
+int dy[4]={0,1,0,-1};
+bool debug=false;
+ 
+/*---------------------------------------------------*/
+
+int main(){
+  int n;
+  map<int,int> data;
+  cin>>n;
+  rep(i,n){
+    int a;cin>>a;
+    data[a]++;
+  }
+  ll ans=0;
+  for(auto itr : data){
+    if(itr.first>itr.second){
+      ans+=itr.second;
+    }else{
+      ans+=(itr.second-itr.first);
+    }
+  }
+  cout<<ans<<endl;
+  return 0;
+}
