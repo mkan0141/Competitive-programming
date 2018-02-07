ba04a27a4b7fc439bfd5c575da4a89b81adf35f5
mkan0141 <kanzume0141@gmail.com>
2018-02-07 23:43:00 +0900

    add A.cpp

---
+++ AtCoder/ARC/049/A.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <ctime>
+#include <list>
+#include <numeric>
+#include <utility>
+#include <ctime>
+#define INF 1000000000
+#define LINF 9000000000000000000
+#define mod 1000000007
+ 
+#define rep(i,n) for(int i=0;i<int(n);i++)
+#define rrep(i,n) for(int i=n-1;i>=0;i--)
+#define REP(i,a,b) for(int i=(a);i<int(b);i++)
+#define all(x) (x).begin(),x.end()
+#define pb push_back
+#define mp make_pair
+#define MOD(x) (x%(mod))
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+
+int dx[4]={1,0,-1,0};
+int dy[4]={0,1,0,-1};
+bool debug=false;
+ 
+/*---------------------------------------------------*/
+
+int main(){
+    int a[4];
+    string s;
+    cin >> s;
+    cin >> a[0] >> a[1] >> a[2] >> a[3];
+    rep(i, s.size() + 1){
+        rep(j, 4){
+            if(i == a[j])cout << '"';
+        }
+        if(i == s.size())break;
+        cout << s[i];
+    }
+    cout << endl;
+    return 0;
+}
+
