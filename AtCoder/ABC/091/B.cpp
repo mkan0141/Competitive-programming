774278c46221ad1e8a13e956b692f7434788d625
mkan0141 <kanzume0141@gmail.com>
2018-03-18 23:35:06 +0900

    add ABC091

---
+++ AtCoder/ABC/091/B.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <climits>
+#include <stack>
+#include <queue>
+#include <set>
+#include <bitset>
+#include <map>
+#include <unordered_map>
+#include <ctime>
+#include <list>
+#include <numeric>
+#include <utility>
+
+#define INF 1000000000
+#define LINF 9000000000000000000
+#define mod 1000000007
+
+#define rep(i,n) for(int i=0;i<int(n);i++)
+#define rrep(i,n) for(int i=n-1;i>=0;i--)
+#define REP(i,a,b) for(int i=(a);i<int(b);i++)
+#define all(x) (x).begin(),x.end()
+#define pb push_back
+#define mp make_pair
+#define MOD(x) (x%(mod))
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+
+int dx[4]={1,0,-1,0};
+int dy[4]={0,1,0,-1};
+int ddx[8]={-1,-1,0,1,1,1,0,-1};
+int ddy[8]={0,1,1,1,0,-1,-1,-1};
+bool debug=false;
+
+/*---------------------------------------------------*/
+
+int main(){
+  map<string,int>mp;
+  int n,m;
+  string x;
+  cin>>n;
+  rep(i,n){
+    cin>>x;
+    mp[x]++;
+  }
+  cin>>m;
+  rep(i,m){
+    cin>>x;
+    mp[x]--;
+  }
+
+  int maxi=0;
+  for(auto itr : mp){
+    maxi=max(maxi,(int)itr.second);
+  }
+  cout<<maxi<<endl;
+  return 0;
+}
+
